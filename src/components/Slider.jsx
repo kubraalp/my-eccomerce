696466433041e98643a464e0193c0546cd7e4ba3
Kubra Alp <kubra.alp.k@gmail.com>
2024-08-22 20:37:27 +0300

    Slider edited

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SliderComponent = ({ images }) => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
 
-    return (
-        <Slider {...settings}>
-            {images.map((image, index) => (
-                <div key={index}>
-                    <img src={image} alt={`Slide ${index}`} className="w-full h-auto" />
-                </div>
-            ))}
-        </Slider>
-    );
+  return (
+    <Slider {...settings}>
+      {images.map((image, index) => (
+        <div key={index} className="relative w-full h-64 overflow-hidden">
+          <img src={image} alt={`Slide ${index}`} className="absolute inset-0 w-full h-full object-cover" />
+        </div>
+      ))}
+    </Slider>
+  );
 };
 
 export default SliderComponent;
